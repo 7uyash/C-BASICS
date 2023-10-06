2862b9baffb1678c6011c0931eb200360c1258b7
Suyash sahu <142486268+7uyash@users.noreply.github.com>
2023-10-06 11:36:17 +0530

    Create Algorithm.c
    simple C algorithm to find the factorial of a given number using recursion

---
+++ Algorithm.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+long long factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    long long result = factorial(num);
+    printf("Factorial of %d is %lld\n", num, result);
+
+    return 0;
+}
